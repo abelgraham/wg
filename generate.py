57d61e4e870b75ec6aa77ba536a78d59c79e81e3
Abel Graham <abel@abelgraham.xyz>
2019-12-10 09:57:53 -0500

    Added blog feature and per-md-file layouts

--- generate.py
+++ generate.py
@@ -1,25 +1,32 @@
 import os
 import json
 import datetime
+import datetime
 import mistune
 from shutil import copyfile
 from jinja2 import Template
 
 BASE_DIR = os.getcwd()
-CONTENT_DIR = os.path.join(BASE_DIR, 'content')
+PAGE_DIR = os.path.join(BASE_DIR, 'pages')
+POST_DIR = os.path.join(BASE_DIR, 'posts')
 OUTPUT_DIR = os.path.join(BASE_DIR, 'docs')
+BLOG_DIR = os.path.join(OUTPUT_DIR, 'blog')
 STYLES_DIR = os.path.join(BASE_DIR, 'styles')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 CONFIG = json.load(open(os.path.join(BASE_DIR, 'config.json')))
 
 atts = {}
 atts['year'] = datetime.datetime.now().year
+atts['blog'] = []
 
-with(open(os.path.join(TEMPLATE_DIR, 'layout.j2'))) as t:
-    TEMPLATE = Template(t.read())
+with(open(os.path.join(TEMPLATE_DIR, 'manifest_page.j2'))) as t:
+    PAGE_TEMPLATE = Template(t.read())
+    
+with(open(os.path.join(TEMPLATE_DIR, 'manifest_post.j2'))) as t:
+    POST_TEMPLATE = Template(t.read())
 
-def get_content():
-    for entry in os.scandir(CONTENT_DIR):
+def get_pages():
+    for entry in os.scandir(PAGE_DIR):
         if all([
             not entry.name.startswith('.'),
             entry.name.endswith('.md'),
@@ -27,34 +34,71 @@ def get_content():
         ]):
             yield entry.name
 
-def generate_html(files):
+def get_posts():
+    for entry in os.scandir(POST_DIR):
+        if all([
+            not entry.name.startswith('.'),
+            entry.name.endswith('.md'),
+            entry.is_file()
+        ]):
+            yield entry.name 
+            
+def generate_html():
     for att in CONFIG:
         atts[att] = CONFIG[att]
         
-    for file in files:
-        with open(os.path.join(CONTENT_DIR, file)) as f:
+    # Generate the HTML for posts and prepare variables for get_pages
+    for file in get_posts():
+        with open(os.path.join(POST_DIR, file)) as f:
+            if os.path.isfile(os.path.join(BASE_DIR, POST_DIR, file.split('.')[0] + ".json")):
+                with(open(os.path.join(TEMPLATE_DIR, file.split('.')[0] + '.j2'))) as t:
+                    template = Template(t.read())
+            else:
+                template = POST_TEMPLATE
+
             atts['content'] = mistune.markdown(f.read())
+            atts['post_title'] = ' '.join(file.split('_')[1].split('.')[0].split('-'))
+            atts['date'] = file.split('_')[0]
 
         new_filename = os.path.splitext(file)[0] + '.html'
         
-        open(os.path.join(OUTPUT_DIR, new_filename), 'w').write(TEMPLATE.render(atts))
+        open(os.path.join(OUTPUT_DIR, BLOG_DIR, new_filename), 'w').write(template.render(atts))
+        atts['blog'].append(
+            {"title": atts['date'] + " / " + atts['post_title'],
+             "link": "blog/" + file.split('.')[0] + ".html"})
         
-def main():
+    # Generate the HTML for pages
+    for file in get_pages():
+        with open(os.path.join(PAGE_DIR, file)) as f:
+            if os.path.isfile(os.path.join(BASE_DIR, PAGE_DIR, file.split('.')[0] + ".json")):
+                with(open(os.path.join(TEMPLATE_DIR, file.split('.')[0] + '.j2'))) as t:
+                    template = Template(t.read())
+            else:
+                template = PAGE_TEMPLATE
+                
+            content = f.read()
+            atts['content'] = mistune.markdown(content) 
+
+        new_filename = os.path.splitext(file)[0] + '.html'
+        
+        open(os.path.join(OUTPUT_DIR, new_filename), 'w').write(template.render(atts))
+
+def main():        
     if not os.path.exists(OUTPUT_DIR):
         os.mkdir(OUTPUT_DIR)
+    if not os.path.exists(BLOG_DIR):
+        os.mkdir(BLOG_DIR)
         
     for entry in os.scandir(STYLES_DIR):
         if all([
-                not entry.name.startswith('.'),
-                entry.name.endswith('.css'),
-                entry.is_file()
+            not entry.name.startswith('.'),
+            entry.name.endswith('.css'),
+            entry.is_file()
         ]):
             copyfile(os.path.join(STYLES_DIR, entry.name), os.path.join(OUTPUT_DIR, entry.name))
-    
+            copyfile(os.path.join(STYLES_DIR, entry.name), os.path.join(BLOG_DIR, entry.name))
         
-    generate_html(get_content())
+    generate_html()
 
-import datetime
 if __name__ == '__main__':
     main()
-
